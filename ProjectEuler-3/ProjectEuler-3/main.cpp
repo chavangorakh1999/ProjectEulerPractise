aba483cb5761c243892312908776f8a855bba07f
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-09-01 20:32:19 +0530

    empty commit

--- ProjectEuler-3/ProjectEuler-3/main.cpp
+++ ProjectEuler-3/ProjectEuler-3/main.cpp
@@ -1,99 +1,99 @@
-////
-////  main.cpp
-////  ProjectEuler-3
-////
-////  Created by Gorakh Chavan on 23/08/20.
-////  Copyright © 2020 Gorakh Chavan. All rights reserved.
-////
 //
-//#include <iostream>
-//#include<cmath>
-//#include<string>
-//using namespace std;
-////void primeFactor(unsigned long long n)
-////{
-////    while(n%2==0)
-////    {
-////        cout<<2<<" ";
-////        n=n/2;
-////    }
-////    for(unsigned long long i=3;i<=sqrt(n);i=i+2)
-////    {
-////        while(n%i==0)
-////        {
-////            cout<<i<<" ";
-////            n=n/i;
-////        }
-////    }
-////    if(n>2)
-////    {
-////        cout<<n<<" ";
-////    }
-////}
-//bool * isPrime(int n)
+//  main.cpp
+//  ProjectEuler-3
+//
+//  Created by Gorakh Chavan on 23/08/20.
+//  Copyright © 2020 Gorakh Chavan. All rights reserved.
+//
+
+#include <iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+//void primeFactor(unsigned long long n)
 //{
-//
-//    bool* array = new bool[100];
-//
-//    for(int i=0;i<n+1;i++)
+//    while(n%2==0)
 //    {
-//        array[i]=true;
+//        cout<<2<<" ";
+//        n=n/2;
 //    }
-//   array[0]=false;
-//   array[1]=false;
-//    for(int i=2;i*i<=n;i++)
+//    for(unsigned long long i=3;i<=sqrt(n);i=i+2)
 //    {
-//        for(int j=2*i;j<=n;j+=i)
+//        while(n%i==0)
 //        {
-//            array[j]=false;
+//            cout<<i<<" ";
+//            n=n/i;
 //        }
 //    }
-//
-//
-//    return array;
-//}
-//
-//int main()
-//{
-////    unsigned long long num;
-////    cin>>num;
-////    int answer=0;
-////    for (int i = 2; num != 1; i++) {
-////        while (num % i == 0) {
-////            num /= i;
-////            answer = i;
-////        }
-////    }
-////    cout<<answer<<endl;
-//////    primeFactor(n);
-////    return 0;
-//
-////    int n;
-////
-//////    char ch;
-////    cout<<"Enter Number of string:";
-////    cin>>n;
-////    cin >> ws;
-////    string arr[n];
-////    for(int i=0;i<n;i++)
-////    {
-////        getline(cin,arr[i]);
-////    }
-////    cout<<endl;
-////    for(int i=0;i<n;i++)
-////    {
-////        cout<<arr[i]<<endl;
-////    }
-////
-//
-//    int n=10;
-////    cout<<"Enter your number";
-////    cin>>n;
-//    bool *p=isPrime(n);
-//    for(int i=1;i<n+1;i++)
+//    if(n>2)
 //    {
-//        cout<<p[i]<<" ";
+//        cout<<n<<" ";
+//    }
+//}
+bool * isPrime(int n)
+{
+    
+    bool* array = new bool[100];
+    
+    for(int i=0;i<n+1;i++)
+    {
+        array[i]=true;
+    }
+   array[0]=false;
+   array[1]=false;
+    for(int i=2;i*i<=n;i++)
+    {
+        for(int j=2*i;j<=n;j+=i)
+        {
+            array[j]=false;
+        }
+    }
+    
+    
+    return array;
+}
+
+int main()
+{
+//    unsigned long long num;
+//    cin>>num;
+//    int answer=0;
+//    for (int i = 2; num != 1; i++) {
+//        while (num % i == 0) {
+//            num /= i;
+//            answer = i;
+//        }
+//    }
+//    cout<<answer<<endl;
+////    primeFactor(n);
+//    return 0;
+    
+//    int n;
+//
+////    char ch;
+//    cout<<"Enter Number of string:";
+//    cin>>n;
+//    cin >> ws;
+//    string arr[n];
+//    for(int i=0;i<n;i++)
+//    {
+//        getline(cin,arr[i]);
+//    }
+//    cout<<endl;
+//    for(int i=0;i<n;i++)
+//    {
+//        cout<<arr[i]<<endl;
 //    }
 //
-//    return 0;
-//}
+    
+    int n=10;
+//    cout<<"Enter your number";
+//    cin>>n;
+    bool *p=isPrime(n);
+    for(int i=1;i<n+1;i++)
+    {
+        cout<<p[i]<<" ";
+    }
+   
+    return 0;
+}
